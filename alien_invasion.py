f031716c152f0e71760c75092d99915604063462
Abtin <abtin.b.farshid@gmail.com>
2026-07-02 18:53:24 +0330

    added moving left and right and exiting with q

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -29,12 +30,35 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme() #drawing ship
         # make the recent screen visible
         pygame.display.flip()
+
+    def _check_keydown_events(self, event):
+        """checks keypresses events and manages them"""
+        if event.key == pygame.K_RIGHT:
+            #only change the flag to True
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q: #for quting faster :)
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """check key releases events and manages them"""
+        if event.key == pygame.K_RIGHT:
+            #set the flag to False
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            #set the flag to False
+            self.ship.moving_left = False
 
 if __name__ == "__main__":
     # make a game instance and run the game
--- ship.py
+++ ship.py
@@ -1,8 +1,11 @@
 import pygame
+from settings import Settings
 
 class Ship:
     """Everything about the ship"""
     def __init__(self, ai_game):
+
+        self.settings = Settings()
         # initialzing the ship and its starting position
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
@@ -14,6 +17,23 @@ class Ship:
         #start at the bottm mid
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.rect.x) # a float for ship x 
+
+        #creat a flag for moving right and left
+        self.moving_right = False
+        self.moving_left = False
+
+        
+
     def blitme(self):
         """Draw ship at the mid bottom of screen"""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """moves right if the flage is true"""
+        #check for lef and right boundaries as well
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
